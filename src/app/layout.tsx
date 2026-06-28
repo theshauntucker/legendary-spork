2866183e6a3b240f13d5f0b49774f4d6e12c50b7
theshauntucker <22tucker22@comcast.net>
2026-06-28 15:41:17 -0700

    Add Meta Pixel (255034242069090) for ad tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -241,6 +241,22 @@ export default async function RootLayout({
             ttq.page();
           }(window, document, 'ttq');
         `}</Script>
+
+        {/* Meta (Facebook) Pixel — id 255034242069090 */}
+        <Script id="meta-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '255034242069090');
+          fbq('track', 'PageView');
+        `}</Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=255034242069090&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
